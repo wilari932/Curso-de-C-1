cf06d9b072c9c6ee6d2331662aac7cf102bfc097
wilmar <wilmar.arias@hotmail.com>
2020-10-31 02:09:14 +0100

    ´google translate

---
+++ GoogleTranslate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+namespace GoogleTranslate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("traductor 1.1:");
+            do
+            {
+                var t = new Translator();
+                var input = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(t.Translate(input));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("si deseas serar el traductor oprime f7 o presiona cualquiera para seguir");
+            } while (Console.ReadKey().Key != ConsoleKey.F7);
+          
+        }
+    }
+}
---
+++ GoogleTranslate/Translator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Web;
+
+namespace GoogleTranslate
+{
+    public class Translator
+    {
+
+        public string Translate(string text, string fromLanguage = "es", string toLanguage = "en")
+        {
+            using (WebClient client = new WebClient())
+            {
+
+
+                string url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromLanguage}&tl={toLanguage}&dt=t&q={HttpUtility.UrlEncode(text)}";
+
+                var result = client.DownloadString(url);
+                try
+                {
+
+                    return result.Substring(4, result.IndexOf("\"", 4, StringComparison.Ordinal) - 4);
+                }
+                catch
+                {
+                    throw new Exception("error with the translator");
+                }
+
+            }
+        }
+    }
+}
